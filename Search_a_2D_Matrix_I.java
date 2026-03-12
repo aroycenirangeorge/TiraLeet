8e88b0d5b8a6728424add8d9281c5d9dd4e78d63
AJ Royce Niran George <135869002+aroycenirangeorge@users.noreply.github.com>
2026-03-12 19:46:58 +0530

    Update Search_a_2D_Matrix_I.java

--- Search_a_2D_Matrix_I.java
+++ Search_a_2D_Matrix_I.java
@@ -1,12 +1,23 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+    public boolean binarySearch(int l, int r, int[][] matrix, int target){
+        if(l>r){
+            return false;
+        }
+        int mid=l+(r-l)/2;
+        int row=mid/matrix[0].length, col=mid%matrix[0].length;
+        if(matrix[row][col]==target){
+            return true;
+        }
+        else if(matrix[row][col]>target){
+            return binarySearch(l,mid-1,matrix,target);
+        }
+        else if(matrix[row][col]<target){
+            return binarySearch(mid+1,r,matrix,target);
         }
         return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int l=0, r=(matrix.length*matrix[0].length)-1;
+        return binarySearch(l,r,matrix,target);
     }
 }

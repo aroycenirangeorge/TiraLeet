f4f79b3d20face643c4a2100324ba3125331f3e4
AJ Royce Niran George <135869002+aroycenirangeorge@users.noreply.github.com>
2026-06-16 10:20:18 +0530

    Create Trapping_Rain_Water.java

---
+++ Trapping_Rain_Water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int l= 0;
+        int r= height.length - 1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+        int water = 0;
+
+        while (l< r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                water += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                water += rightMax - height[r];
+            }
+        }
+
+        return water;        
+    }
+}
